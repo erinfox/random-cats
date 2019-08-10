5b04fbb96ed672b0fa9f37656822811f0f40c8a7
Erin Fox <erinfox@Erins-MacBook-Pro.local>
2019-08-09 17:52:07 -0700

    pink button

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {StyleSheet, Text, View, TouchableOpacity} from "react-native";
 
 export default function App() {
+  handleClick = () => {
+    // alert("Button clicked!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        style={styles.button}
+        onPress={this.handleClick}>
+        <Text>Click for Cats</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  button: {
+    height: 70,
+    width: "80%",
+    alignItems: "center",
+    backgroundColor: "#FFB6C1",
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {}
 });
